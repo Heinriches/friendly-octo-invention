774a1814ab8dfa5b0ffcce2187ab16a4d3e9ae38
Heinriches <heydrich.institut@gmail.com>
2025-09-18 20:32:39 -0300

    Update BalancoTrimestral.java

--- (book) Java e Orientacao a Objetos FJ-11/cap 03/3.3/Exercicio_01/BalancoTrimestral.java
+++ (book) Java e Orientacao a Objetos FJ-11/cap 03/3.3/Exercicio_01/BalancoTrimestral.java
@@ -2,6 +2,8 @@ class BalancoTrimestral{
 	public static void main(String[] args){
 		public static void main (String[] args){
 			int gastosJaneiro = 15000;
+			int gastosFevereiro = 23000;
+			int gastosMarco = 17000;
 		}
 	}
 }
