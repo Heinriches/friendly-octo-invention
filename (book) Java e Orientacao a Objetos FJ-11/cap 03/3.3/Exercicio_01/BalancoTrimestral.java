a4998b7d820abeabcc7153f7dcb609ae52d5646a
Heinriches <heydrich.institut@gmail.com>
2025-09-18 20:46:23 -0300

    Update BalancoTrimestral.java

--- (book) Java e Orientacao a Objetos FJ-11/cap 03/3.3/Exercicio_01/BalancoTrimestral.java
+++ (book) Java e Orientacao a Objetos FJ-11/cap 03/3.3/Exercicio_01/BalancoTrimestral.java
@@ -5,7 +5,5 @@ class BalancoTrimestral{
 		int gastosMarco = 17000;
 		int gastosTrimestre = (gastosJaneiro + gastosFevereiro + gastosMarco);
 		System.out.println(gastosTrimestre);
-		float mediaMensal = (gastosJaneiro + gastosFevereiro + gastosMarco) / 3;
-		System.out.println("Valor da média-mensal = " +mediaMensal);
 	}
 }
