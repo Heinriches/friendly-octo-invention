af3add3fde3a4676885d4f9766b2ab3d033613aa
Heinriches <heydrich.institut@gmail.com>
2025-09-22 11:47:18 -0300

    Update Pos_Incremento.java

--- (book) Java e Orientacao a Objetos FJ-11/cap 03/3.8/Pos_Incremento.java
+++ (book) Java e Orientacao a Objetos FJ-11/cap 03/3.8/Pos_Incremento.java
@@ -1,8 +1,8 @@
 class Pos_Incremento{
 	public static void main(String[] args){
 		int i = 5;
-		int x = i+;
-		int y = +i;
+		int x = i++;
+		int y = ++i;
 		System.out.println("Pós-incremento (x = i+) = " +x);
 		System.out.println("Pré-incremento (y = +i) = " +y);
 	}
